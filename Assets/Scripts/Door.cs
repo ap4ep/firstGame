569aa239ebce5fa385f026c58acc9125a23e3ec3
Ilya Naumov <ap4@live.ru>
2021-02-24 00:40:15 +0500

    Add Jump point for Nav Mesh Agent and trap

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             gameObject.SetActive(false);
